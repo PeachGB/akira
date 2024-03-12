39ddc47b7f1c3c56196756d86347c58dab03eb39
PeachGB <arianmateos@gmail.com>
2024-03-11 21:39:15 -0300

    ready to boot

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,19 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {loop{}}
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] //this atribute is so the compiler outputs a function with the name "_start" very
 pub extern "C" fn _start() -> ! { //for the "extern "C"" explanation, google "C calling convention"
-    loop{}}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe{
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop{}
+}
 
 
