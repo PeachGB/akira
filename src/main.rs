434d53666e7c0c896a3e85e5ed22703f335489a6
Arián <136646282+PeachGB@users.noreply.github.com>
2025-08-09 23:16:45 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-//The std requires an OS for syscalls so we disable it.
-//Also the fn main requires the STD so we disable it as well 
+
 #![no_std]
 #![no_main]
 
@@ -11,8 +10,8 @@ fn panic(_info: &PanicInfo) -> ! {loop{}}
 
 static HELLO: &[u8] = b"Hello World!";
 
-#[no_mangle] //this atribute is so the compiler outputs a function with the name "_start" very
-pub extern "C" fn _start() -> ! { //for the "extern "C"" explanation, google "C calling convention"
+#[no_mangle] 
+pub extern "C" fn _start() -> ! { 
     // let vga_buffer = 0xb8000 as *mut u8;
 
     // for (i, &byte) in HELLO.iter().enumerate(){
